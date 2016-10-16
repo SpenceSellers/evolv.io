bf373135347d4b2d734614404370842c96c95671
Spencer Sellers <spencesellers@gmail.com>
2016-10-16 14:43:30 -0500

    Minor Prox optimization; unsure if it helped

--- EvolvioColor/Prox.java
+++ EvolvioColor/Prox.java
@@ -1,39 +1,48 @@
 import java.util.*;
+import java.util.Collections;
 
 class Prox<T> {
   private class ProxData<T> {
     public double x;
     public double y;
-    public Set<T> near;
+    public Set<T> near = null;
     
-    ProxData(double x, double y, Set<T> near){
+    ProxData(double x, double y){
       this.x = x;
       this.y = y;
-      this.near = near;
+    }
+    
+    private void addNear(T t){
+      if (this.near == null){
+        this.near = new HashSet();
+      }
+      this.near.add(t);
     }
   }
   
   private Map<T, ProxData<T>> map = new HashMap();
   
   public void add(double x, double y, T item){
-    Set<T> nearUs = new HashSet();
+    ProxData<T> ourProx = new ProxData(x, y);
     for (Map.Entry<T, ProxData<T>> e : this.map.entrySet()){
       double otherx = e.getValue().x;
       double othery = e.getValue().y;
       if (Math.abs(x - otherx) < 1.0 && Math.abs(y - othery) < 1.0){
-        nearUs.add(e.getKey());
-        e.getValue().near.add(item);
+        ourProx.addNear(e.getKey());
+        e.getValue().addNear(item);
       }
     }
     
-    this.map.put(item, new ProxData(x, y, nearUs));
+    this.map.put(item, ourProx);
   }
   
-  //private void addEmpty(T t){
-  //  this.map.put(t, new TreeSet());
-  //}
   
   public Set<T> get(T t){
-    return this.map.get(t).near;
+    Set<T> res = this.map.get(t).near;
+    if (res == null){
+      return Collections.EMPTY_SET;
+    } else {
+      return res;
+    }
   }
 }
